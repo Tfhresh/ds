aaaac3da3864ce59a1381cad97e37bfd0a5294f4
Temiloluwa Adebayo <temiadebayo15.com>
2025-10-27 10:17:15 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,80 @@
+let num = [1, 2, 3];
+
+const ArrayList = {
+  length: 0,
+  arr: [],
+  size: 100
+};
+
+function insertAtEnding(List, newvalue) {
+  if (List.size === List.length) {
+    console.log("List is Full");
+    return;
+  }
+
+  List.arr[List.length] = newvalue;
+  List.length += 1;
+}
+
+function insertAtBeginning(List, newValue) {
+  if (List.size === List.length) {
+    console.log("List is Full");
+    return;
+  }
+
+  let i = List.length;
+  for(; i>0; i--) {
+    List.arr[i] = List.arr[i-1];
+  }
+  List.arr[i] = newValue;
+  List.length += 1;
+
+  // let i = List.length-1;
+  // for(; i>=0; i--) {
+  //   List.arr[i+1] = List.arr[i];
+  // }
+  // List.arr[0] = newValue;
+}
+
+function insertAtAPos(List, pos, newValue) {
+
+  if(pos < 0 || pos > List.length - 1) {
+    return;
+  }
+  for(let i = List.length-1; i > pos; i--) {
+    List.arr[i] = List.arr[i-1];
+  }
+  List.arr[pos] = newValue; 
+}
+
+function displayArray(List) {
+  let list = '';
+  for(let i=0; i<List.length; i++) {
+    list += `${List.arr[i]} `;
+  }
+  console.log(list);
+}
+
+
+
+insertAtEnding(ArrayList, 2);
+insertAtEnding(ArrayList, 4);
+insertAtBeginning(ArrayList, 65);
+insertAtBeginning(ArrayList, 70);
+insertAtAPos(ArrayList, 1, 7); 
+displayArray(ArrayList);
+
+
+
+/*
+List made with array.
+
+Operations
+ - insertion
+ - deletion
+ - display
+ - transversal
+ - find
+ - modify
+ - merge
+*/ 
