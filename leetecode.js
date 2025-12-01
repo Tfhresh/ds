a51d3eb5b5e34ee218bafcc9ad823f19010353b5
Temiloluwa Adebayo <temiadebayo15.com>
2025-12-01 09:47:56 +0000

    Worked on factorials, rotation, reversal and palindrome

--- leetecode.js
+++ leetecode.js
@@ -43,3 +43,106 @@ function reverseNum2(num) {
   return reverse;
 }
 
+function reverseArr(arr) {
+  const reverse = [];
+  for (let i = arr.length - 1, j = 0; i >= 0; i--, j++) {
+    reverse[j] = arr[i];
+  }
+
+  console.log(reverse);
+}
+
+function swapWiththirdVariable(num1, num2) {
+  let temp = num1;
+  num1 = num2;
+  num2 = temp;
+
+  return [num1, num2];
+}
+
+function swapNumbers(num1, num2) {
+  num1 = num1 + num2;
+  num2 = num1 - num2;
+  num1 = num1 - num2;
+
+  return [num1, num2];
+}
+
+function inPlaceReverse(arr) {
+  let high = arr.length - 1;
+  let low = 0;
+
+  while (low <= high) {
+    let temp = arr[low];
+    arr[low] = arr[high];
+    arr[high] = temp;
+
+    // const swap = swapNumbers(arr[high], arr[low]);
+    // arr[low] = swap[1];
+    // arr[high] = swap[0];
+
+    low++;
+    high--;
+  }
+  return arr;
+}
+
+function rotateArrLeft(arr) {
+  let first = arr[0];
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    arr[i] = arr[i + 1];
+  }
+
+  arr[arr.length - 1] = first;
+
+  return arr;
+}
+
+function rotateArrRight(arr) {
+  let last = arr[arr.length - 1];
+
+  for (let i = arr.length - 1; i > 0; i--) {
+    arr[i] = arr[i - 1];
+  }
+
+  arr[0] = last;
+  return arr;
+}
+
+function iterativeFactorial(n) {
+  let factorial = 1;
+  for (let i = 1; i <= n; i++) {
+    factorial *= i;
+  }
+
+  return factorial;
+}
+
+function recursiveFactorial(n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+
+  return n * recursiveFactorial(n - 1);
+}
+
+function reduceFactorial (n) {
+  return Array.from(new Array(n)).reduce((acc, _, curIn) => acc * (curIn + 1), 1);
+}
+
+function isPalindrome(number) {
+
+  let num = number;
+  let reverse = 0;
+  let length = numberLength(num)-1;
+
+  while(num !== 0) {
+    let rem = num % 10;
+    reverse += (rem * (10**(length--)));
+    num = Math.floor(num/10);
+  }
+
+  return reverse === number;
+}
+
